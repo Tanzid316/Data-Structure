f0f4b262dfb2365f8c46da9a68e42f05c3d2588a
Sheikh Tanzid Ahmed Sadi <tanzid01716@gmail.com>
2026-06-30 09:15:03 +0600

    Refactor InsertionSort implementation

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -4,25 +4,24 @@ vector<int> vec;
 
 void insertionSort()
 {
-    int i, key, j;
-    for(i = 1; i < vec.size(); i++)
+    int i,j,key;
+    for(i=1; i<vec.size(); i++)
     {
-        key = vec[i];      // Store the current element to insert
-        j = i - 1;         // Start comparing with the last element of sorted portion
+        key = vec[i];
+        j=i-1;
 
-        // Shift elements of sorted portion that are greater than key
-        // to one position ahead of their current position
-        while(j >= 0 && vec[j] > key)
+        while(j>=0 && vec[j] > key)
         {
-            vec[j + 1] = vec[j];  // Move element right
-            j--;                  // Move left to check next element
+            vec[j+1] = vec[j];
+            j--;
+            
         }
-
-        vec[j + 1] = key;  // Place key in its correct position
+        
+        vec[j+1] = key;
     }
 }
 
-// Helper function to print the vector
+
 void printVector()
 {
     for(int i = 0; i < vec.size(); i++)
